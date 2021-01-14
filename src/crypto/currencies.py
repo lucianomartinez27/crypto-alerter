2b6ee5fc2b1cb5e84e79e18f9345a2c54633dd1a
Luciano Martinez <lucianomartinez27@gmail.com>
2021-01-14 20:20:13 -0300

    cryptocurrencies has update date

--- src/crypto/currencies.py
+++ src/crypto/currencies.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from datetime import date
+
 
 class CryptoCurrency:
     def __init__(self, symbol, price):
         # this is sometimes called ticker (like 'BTC', 'ETH', 'USD', etc.)
         self.symbol = symbol
         self.price = price
+        self.last_updated = date.today()
--- src/crypto/tests/test_01.py
+++ src/crypto/tests/test_01.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 import unittest
+from datetime import date
 
 from ..currencies import CryptoCurrency
 
 
-class Test01(unittest.TestCase):
+class TestCryptoCurrencies(unittest.TestCase):
     def setUp(self) -> None:
         self.btc = CryptoCurrency('BTC', 1)
         self.eth = CryptoCurrency('ETH', 2)
@@ -19,8 +20,11 @@ class Test01(unittest.TestCase):
         self.assertIsNotNone(self.eth.price)
 
     def test_CryptoCurrencies_has_his_own_price(self):
-
         self.assertNotEqual(self.btc.price, self.eth.price)
+
+    def test_cryptocurrencies_has_update_date(self):
+        self.assertEqual(self.btc.last_updated, date.today())
+        self.assertEqual(self.eth.last_updated, date.today())
 
 if __name__ == '__main__':
     unittest.main()
