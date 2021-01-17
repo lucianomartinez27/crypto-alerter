0d261882cc3a1b612a98443ab9105a7901940bb0
Luciano Martinez <lucianomartinez27@gmail.com>
2021-01-17 15:50:52 -0300

    test add event to listener and execute action

---
+++ src/crypto/listener.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from src.crypto.event import Event
+
+
+class Listener:
+    def __init__(self):
+        self.events = []
+
+    def add_event(self, alert, action):
+        self.events.append(Event(alert, action))
+
+    def check_alerts(self):
+        for event in self.events:
+            event.execute_action()
---
+++ src/crypto/tests/test_listener.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import unittest
+from unittest import mock
+from unittest.mock import Mock
+
+from src.crypto.currencies import CryptoCurrency
+from src.crypto.listener import Listener
+from src.crypto.price_alerter import PriceAlert
+
+
+class TestAddEventToListenerAndExecuteAction(unittest.TestCase):
+    def setUp(self) -> None:
+        self.listener = Listener()
+
+    @mock.patch('src.crypto.price_alerter.PriceAlert')
+    def test_listener_can_add_event_and_check_alerts(self, mocked_price_alert):
+        self.listener.add_event(mocked_price_alert, lambda: 'pass')
+        self.listener.add_event(PriceAlert(CryptoCurrency('ETH', 10), lambda price: price > 10), lambda: 'pass')
+        self.listener.check_alerts()
+        self.assertEqual(self.listener.events[0].price_alert.matches.call_count, 1)
+
+    @mock.patch('src.crypto.price_alerter.PriceAlert')
+    def test_when_alert_is_checked_action_is_executed_if_corresponds(self, mocked_price_alert):
+        action = Mock()
+        another_action = Mock()
+        self.listener.add_event(mocked_price_alert, action)
+        self.listener.add_event(PriceAlert(CryptoCurrency('ETH', 10), lambda price: price >= 20), another_action)
+        self.listener.check_alerts()
+        self.assertEqual(self.listener.events[0].action.call_count, 1)
+        self.assertEqual(self.listener.events[1].action.call_count, 0)
+
